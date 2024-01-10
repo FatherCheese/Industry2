9dbd113146a35f256c5e42cf8165127a45cd2f61
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-01-10 08:21:59 -0600

    reimplemented trommel additions

---
+++ src/main/java/baboon/industry/recipe/RecipesTrommel.java
@@ -0,0 +1,25 @@
+package baboon.industry.recipe;
+
+import baboon.industry.item.IndustryItems;
+import net.minecraft.core.WeightedRandomLootObject;
+import net.minecraft.core.data.registry.Registries;
+import net.minecraft.core.item.ItemStack;
+import turniplabs.halplibe.util.RecipeEntrypoint;
+
+public class RecipesTrommel implements RecipeEntrypoint {
+    @Override
+    public void onRecipesReady() {
+        Registries.RECIPES.TROMMEL.getItem("clay").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawUranium), 1), 1.0);
+        Registries.RECIPES.TROMMEL.getItem("dirt").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawTin), 1), 0.5f);
+        Registries.RECIPES.TROMMEL.getItem("dirt").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawCopper), 1), 0.5f);
+        Registries.RECIPES.TROMMEL.getItem("gravel").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawTin), 1, 2), 10.0f);
+        Registries.RECIPES.TROMMEL.getItem("gravel").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawCopper), 1, 2), 10.0f);
+        Registries.RECIPES.TROMMEL.getItem("rich_dirt").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawTin), 1, 2), 15.0f);
+        Registries.RECIPES.TROMMEL.getItem("rich_dirt").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawCopper), 1, 2), 15.0f);
+        Registries.RECIPES.TROMMEL.getItem("rich_dirt").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawUranium), 1), 1.0);
+        Registries.RECIPES.TROMMEL.getItem("sand").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawUranium), 1), 1.0);
+        Registries.RECIPES.TROMMEL.getItem("soul_sand").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawTin), 1), 1.0f);
+        Registries.RECIPES.TROMMEL.getItem("soul_sand").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawCopper), 1), 1.0f);
+        Registries.RECIPES.TROMMEL.getItem("soul_sand").getOutput().addEntry(new WeightedRandomLootObject(new ItemStack(IndustryItems.oreRawUranium), 1), 2.0);
+    }
+}
